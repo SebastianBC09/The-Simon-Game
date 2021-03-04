af521cde95456a320df83ef259687673aa1ac0a8
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-03-04 16:04:59 -0500

    Query selector using jquery to animate the buttons when the random number is equal to its name. Then audio variable created to play the corresponding file to its color name.

--- game.js
+++ game.js
@@ -5,5 +5,11 @@ function nextSecuence() {
   var randomNumber = Math.floor(Math.random() * 4)
   var randomChosenColour = buttonColours[randomNumber]
   gamePattern.push(randomChosenColour)
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
+  
+  var audio = new Audio('sounds/' + randomChosenColour + '.mp3')
+  audio.play() 
 }
+
 
