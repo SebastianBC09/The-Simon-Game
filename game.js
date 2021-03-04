02e8c606d9eaf7f547bb4f1136277dd4b43a1be2
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-03-04 15:35:36 -0500

    Variables & arrays created, function created with random Number generator.

--- game.js
+++ game.js
@@ -1,1 +1,9 @@
-alert("Hello")
+var buttonColours = ["red", "blue", "green", "yellow"]
+var gamePattern = []
+
+function nextSecuence() {
+  var randomNumber = Math.floor(Math.random() * 4)
+  var randomChosenColour = buttonColours[randomNumber]
+  gamePattern.push(randomChosenColour)
+}
+
