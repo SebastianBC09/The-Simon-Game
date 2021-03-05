3e69e288b711115f07ffed21cd73c11185d6dcb2
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-03-05 10:49:34 -0500

    Function that creates an animation every time a button is clicked has been created & added to the event listener to add & remove the class.

--- game.js
+++ game.js
@@ -2,10 +2,13 @@ var buttonColours = ["red", "blue", "green", "yellow"]
 var gamePattern = []
 var userClickedPattern = []
 
+var level = 0
+
 $(".btn").click(function() {
   var userChosenColour = $(this).attr("id")
   userClickedPattern.push(userChosenColour)
   playSound(userChosenColour)
+  animatePress(userChosenColour)
 })
 
 function nextSecuence() {
@@ -22,3 +25,9 @@ function playSound(name) {
   audio.play()
 }
 
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed")
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed")
+  }, 100)
+}
