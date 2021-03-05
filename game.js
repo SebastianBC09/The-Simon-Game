786b492e286b6ae9359295246e026cbabf12a49c
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-03-05 11:54:25 -0500

    Function created to compare the user's response with the pattern created. If statement to check it & continue to the next level. If not, play audio, add class & change title that the game is over

--- game.js
+++ game.js
@@ -1,17 +1,52 @@
 var buttonColours = ["red", "blue", "green", "yellow"]
 var gamePattern = []
 var userClickedPattern = []
-
+var gameStarted = false
 var level = 0
 
-$(".btn").click(function() {
-  var userChosenColour = $(this).attr("id")
-  userClickedPattern.push(userChosenColour)
-  playSound(userChosenColour)
-  animatePress(userChosenColour)
+$(document).keydown(function () {
+  if (!gameStarted) {
+    $("#level-title").text("Level " + level)
+    nextSecuence()
+    gameStarted = true
+  }
 })
 
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id")
+  userClickedPattern.push(userChosenColour)
+
+  playSound(userChosenColour)
+  animatePress(userChosenColour)
+
+  checkAnswer(userClickedPattern.lastIndexOf(userChosenColour))
+})
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("Success")
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSecuence()
+      }, 1000);
+    }
+  }else {
+    console.log("Wrong")
+    var audio = new Audio('sounds/wrong.mp3')
+    audio.play()
+    $("#level-title").text("Game Over, Press Any Key to Restart")
+    $("body").addClass("game-over")
+    setTimeout(function () {
+      $("body").removeClass("game-over")
+    }, 200);
+  }
+}
+
 function nextSecuence() {
+  userClickedPattern = []
+  level++
+  $("#level-title").text("Level " + level)
+
   var randomNumber = Math.floor(Math.random() * 4)
   var randomChosenColour = buttonColours[randomNumber]
   gamePattern.push(randomChosenColour)
@@ -27,7 +62,7 @@ function playSound(name) {
 
 function animatePress(currentColour) {
   $("#" + currentColour).addClass("pressed")
-  setTimeout(function() {
+  setTimeout(function () {
     $("#" + currentColour).removeClass("pressed")
   }, 100)
 }
